2d5af2cc9dd88273ade7fca7a81e29fe59b4a178
Ritsuki Goto <kawakawaritsuki.dev@gmail.com>
2016-06-04 23:08:09 +0900

    [add] release v0.2

--- src/main/java/com/kawakawaplanning/japanecraft/JapaneCraftMod.java
+++ src/main/java/com/kawakawaplanning/japanecraft/JapaneCraftMod.java
@@ -17,11 +17,11 @@ import java.util.Map;
 
 @Mod(modid = JapaneCraftMod.MODID, version = JapaneCraftMod.VERSION)
 public class JapaneCraftMod {
-    public static final String MODID = "JapaneCraft";
-    public static final String VERSION = "0.1";
+    public static final String MODID = "japanecraft";
+    public static final String VERSION = "0.2";
 
     boolean isServer = true;
-    
+
     @EventHandler
     public void init(FMLInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(this);
