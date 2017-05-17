4d38597447e5ba7f4125a7613993361235d0de98
wcaokaze <wcaokaze@gmail.com>
2017-05-17 17:33:35 +0900

    indent

--- src/main/kotlin/com/wcaokaze/japanecraft/RomajiConverter.kt
+++ src/main/kotlin/com/wcaokaze/japanecraft/RomajiConverter.kt
@@ -4,11 +4,11 @@ import java.util.*
 
 class RomajiConverter {
   private val romajiTable = trieOf(
-    "-"   to Output("ー"),
-    "zh"  to Output("←"),
-    "qq"  to Output("っ", nextInput = "q"),
-    "ww"  to Output("っ", nextInput = "w"),
-    "www" to Output("ｗ", nextInput = "ww")
+      "-"   to Output("ー"),
+      "zh"  to Output("←"),
+      "qq"  to Output("っ", nextInput = "q"),
+      "ww"  to Output("っ", nextInput = "w"),
+      "www" to Output("ｗ", nextInput = "ww")
   )
 
   private class Output(val jpChar: String, val nextInput: CharSequence = "")
