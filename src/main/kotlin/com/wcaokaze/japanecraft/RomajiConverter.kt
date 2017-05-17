b8ad2bf32238a8b40e091b777f272cfd357fee47
wcaokaze <wcaokaze@gmail.com>
2017-05-17 17:33:01 +0900

    revert

--- src/main/kotlin/com/wcaokaze/japanecraft/RomajiConverter.kt
+++ src/main/kotlin/com/wcaokaze/japanecraft/RomajiConverter.kt
@@ -3,28 +3,13 @@ package com.wcaokaze.japanecraft
 import java.util.*
 
 class RomajiConverter {
-  private val romajiTable = buildRomajiTable {
-    // | input | output | nextInput |
-    // |-------+--------+-----------|
-    add("-"    , "ー"               )
-    add("zh"   , "←"               )
-    add("qq"   , "っ"   , "q"       )
-    add("ww"   , "っ"   , "w"       )
-    add("www"  , "ｗ"   , "ww"      )
-  }
+  private val romajiTable = trieOf(
+    "-"   to Output("ー"),
+    "zh"  to Output("←"),
+    "qq"  to Output("っ", nextInput = "q"),
+    "ww"  to Output("っ", nextInput = "w"),
+    "www" to Output("ｗ", nextInput = "ww")
+  )
 
-  private class Output(val jpChar: String, val nextInput: CharSequence)
-
-  private class RomajiTableBuilder {
-    private val romajiData = LinkedList<Pair<String, Output>>()
-
-    fun add(input: String, output: String, nextInput: CharSequence = "") {
-      romajiData += Pair(input, Output(output, nextInput))
-    }
-
-    fun build() = trieOf(*romajiData.toTypedArray())
-  }
-
-  private fun buildRomajiTable(buildOperation: RomajiTableBuilder.() -> Unit)
-      = RomajiTableBuilder().apply(buildOperation).build()
+  private class Output(val jpChar: String, val nextInput: CharSequence = "")
 }
