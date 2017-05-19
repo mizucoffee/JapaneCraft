691c3d8652d7563591c7458aadacc2d66fdc0a08
wcaokaze <wcaokaze@gmail.com>
2017-05-19 19:54:06 +0900

    implemented basic

--- src/main/kotlin/com/wcaokaze/japanecraft/JapaneCraftMod.kt
+++ src/main/kotlin/com/wcaokaze/japanecraft/JapaneCraftMod.kt
@@ -1,20 +1,15 @@
 package com.wcaokaze.japanecraft
 
-import com.kawakawaplanning.japanecraft.Kana
 import cpw.mods.fml.common.FMLCommonHandler
 import cpw.mods.fml.common.Mod
 import cpw.mods.fml.common.event.FMLInitializationEvent
 import cpw.mods.fml.common.eventhandler.SubscribeEvent
-import cpw.mods.fml.common.network.NetworkCheckHandler
-import cpw.mods.fml.relauncher.Side
 import net.minecraft.util.ChatComponentText
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.event.ServerChatEvent
 
 @Mod(modid = "japanecraft", version = "0.3.0")
 class JapaneCraftMod {
-  private var isServer = false
-
   @Mod.EventHandler
   fun init(event: FMLInitializationEvent) {
     MinecraftForge.EVENT_BUS.register(this)
@@ -22,28 +17,18 @@ class JapaneCraftMod {
 
   @SubscribeEvent
   fun onServerChat(event: ServerChatEvent) {
-    if (isServer) return
-
-    val k = Kana()
-    k.line = event.message
-    k.convert()
-
-    val chatComponentText = ChatComponentText(
-        "<${ event.username }> ${ event.message } (${ k.line })"
-    )
-
-    FMLCommonHandler
+    fun sendChatMsg(msg: String) = FMLCommonHandler
         .instance()
         .minecraftServerInstance
         .configurationManager
-        .sendChatMsg(chatComponentText)
+        .sendChatMsg(ChatComponentText(msg))
+
+    val enMsg = event.message
+    val jpMsg = RomajiConverter.convert(enMsg)
+
+    sendChatMsg("<${ event.username }> $enMsg")
+    sendChatMsg("  §b$jpMsg")
 
     event.isCanceled = true
-  }
-
-  @NetworkCheckHandler
-  fun netCheckHandler(mods: Map<String, String>, side: Side): Boolean {
-    isServer = side.isServer
-    return true
   }
 }
