535a7720801958f12c35f412e8db4c76cc5becb7
wcaokaze <wcaokaze@gmail.com>
2017-05-26 08:08:06 +0900

    implemented except parse function

--- src/main/kotlin/com/wcaokaze/japanecraft/VariableExpander.kt
+++ src/main/kotlin/com/wcaokaze/japanecraft/VariableExpander.kt
@@ -1,7 +1,34 @@
 package com.wcaokaze.japanecraft
 
-class VariableExpander(private val strWithVar: String) {
+class VariableExpander {
+  private val tokenExpanderList: List<TokenExpander>
+
+  constructor(strWithVars: String) {
+
+  }
+
   fun expand(variableMap: Map<String, String>): String {
-    throw NotImplementedError()
+    return tokenExpanderList
+        .map { it.expand(variableMap) }
+        .fold(StringBuffer()) { buffer, token -> buffer.append(token) }
+        .toString()
+  }
+
+  private sealed class TokenExpander {
+    abstract fun expand(variableMap: Map<String, String>): String
+
+    private class ConstantString(private val str: String) : TokenExpander() {
+      override fun expand(variableMap: Map<String, String>): String {
+        return str
+      }
+    }
+
+    private class VariableExpander
+        (private val variableName: String) : TokenExpander()
+    {
+      override fun expand(variableMap: Map<String, String>): String {
+        return variableMap.getOrDefault(variableName, variableName)
+      }
+    }
   }
 }
