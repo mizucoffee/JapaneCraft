682fdfc47f2a54976b311c7d25f7f3e2a636d961
wcaokaze <wcaokaze@gmail.com>
2017-05-29 04:44:12 +0900

    refactored

--- src/main/kotlin/com/wcaokaze/util/JsonParser.kt
+++ src/main/kotlin/com/wcaokaze/util/JsonParser.kt
@@ -1,25 +1,26 @@
 package com.wcaokaze.util
 
 import java.io.InputStream
-import kotlin.Boolean          as KBoolean
-import kotlin.Number           as KNumber
-import kotlin.String           as KString
-import kotlin.collections.List as KList
-import kotlin.collections.Map  as KMap
+
+private typealias KNumber  = kotlin.Number
+private typealias KBoolean = kotlin.Boolean
+private typealias KString  = kotlin.String
+private typealias KList    = kotlin.collections.List<JsonDatum<*>>
+private typealias KMap     = kotlin.collections.Map<KString, JsonDatum<*>>
 
 sealed class JsonDatum<out T>(val value: T) {
-  class  Number (value: KNumber)                     : JsonDatum<KNumber>                    (value)
-  class  String (value: KString)                     : JsonDatum<KString>                    (value)
-  class  Boolean(value: KBoolean)                    : JsonDatum<KBoolean>                   (value)
-  class  List   (value: KList<JsonDatum<*>>)         : JsonDatum<KList<JsonDatum<*>>>        (value)
-  class  Map    (value: KMap<KString, JsonDatum<*>>) : JsonDatum<KMap<KString, JsonDatum<*>>>(value)
-  object Null                                        : JsonDatum<Nothing?>                   (null)
+  class  Number (value: KNumber)  : JsonDatum<KNumber>  (value)
+  class  String (value: KString)  : JsonDatum<KString>  (value)
+  class  Boolean(value: KBoolean) : JsonDatum<KBoolean> (value)
+  class  List   (value: KList)    : JsonDatum<KList>    (value)
+  class  Map    (value: KMap)     : JsonDatum<KMap>     (value)
+  object Null                     : JsonDatum<Nothing?> (null)
 
-  fun getNumber (default: () -> KNumber)                     = if (this is Number)  value else default()
-  fun getString (default: () -> KString)                     = if (this is String)  value else default()
-  fun getBoolean(default: () -> KBoolean)                    = if (this is Boolean) value else default()
-  fun getList   (default: () -> KList<JsonDatum<*>>)         = if (this is List)    value else default()
-  fun getMap    (default: () -> KMap<KString, JsonDatum<*>>) = if (this is Map)     value else default()
+  fun getNumber (default: () -> KNumber)  = if (this is Number)  value else default()
+  fun getString (default: () -> KString)  = if (this is String)  value else default()
+  fun getBoolean(default: () -> KBoolean) = if (this is Boolean) value else default()
+  fun getList   (default: () -> KList)    = if (this is List)    value else default()
+  fun getMap    (default: () -> KMap)     = if (this is Map)     value else default()
   fun getNull() = null
 }
 
