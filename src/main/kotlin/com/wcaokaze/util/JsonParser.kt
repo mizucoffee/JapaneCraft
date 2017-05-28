5db1e7c34057cf053c385dde44e5076d77c52b6e
wcaokaze <wcaokaze@gmail.com>
2017-05-29 04:40:36 +0900

    added utils

--- src/main/kotlin/com/wcaokaze/util/JsonParser.kt
+++ src/main/kotlin/com/wcaokaze/util/JsonParser.kt
@@ -14,6 +14,13 @@ sealed class JsonDatum<out T>(val value: T) {
   class  List   (value: KList<JsonDatum<*>>)         : JsonDatum<KList<JsonDatum<*>>>        (value)
   class  Map    (value: KMap<KString, JsonDatum<*>>) : JsonDatum<KMap<KString, JsonDatum<*>>>(value)
   object Null                                        : JsonDatum<Nothing?>                   (null)
+
+  fun getNumber (default: () -> KNumber)                     = if (this is Number)  value else default()
+  fun getString (default: () -> KString)                     = if (this is String)  value else default()
+  fun getBoolean(default: () -> KBoolean)                    = if (this is Boolean) value else default()
+  fun getList   (default: () -> KList<JsonDatum<*>>)         = if (this is List)    value else default()
+  fun getMap    (default: () -> KMap<KString, JsonDatum<*>>) = if (this is Map)     value else default()
+  fun getNull() = null
 }
 
 fun parseJson(inputStream: InputStream): JsonDatum<*> = throw NotImplementedError()
