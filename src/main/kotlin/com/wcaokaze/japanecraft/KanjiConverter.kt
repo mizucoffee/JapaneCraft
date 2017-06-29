ffcb8d8ebb238dd08aa7aee89b8b102d31999abd
wcaokaze <wcaokaze@gmail.com>
2017-06-30 06:19:35 +0900

    implemented

--- src/main/kotlin/com/wcaokaze/japanecraft/KanjiConverter.kt
+++ src/main/kotlin/com/wcaokaze/japanecraft/KanjiConverter.kt
@@ -4,13 +4,8 @@ import com.wcaokaze.json.*
 import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.Deferred
 import kotlinx.coroutines.experimental.async
-import kotlinx.coroutines.experimental.runBlocking
 import java.net.URL
-
-fun main(vararg args: String) = runBlocking {
-  KanjiConverter().convert(listOf("りんご", "ばなな", "あるみかん")).await()
-      .forEach(::println)
-}
+import java.net.URLEncoder
 
 class KanjiConverter {
   private class GoogleCgiEntry(val hiragana: String, val kanjiList: List<String>)
@@ -25,10 +20,16 @@ class KanjiConverter {
 
   suspend fun convert(hiraganaList: List<String>): Deferred<List<String>>
       = async (CommonPool) {
-        val googleCgiEntryList = URL("http://www.google.com/transliterate?langpair=ja-Hira|ja&text=%E3%81%B8%E3%82%93%E3%81%8B%E3%82%93")
+        val encodedHiraganaList = hiraganaList
+            .map { URLEncoder.encode(it, "UTF-8") }
+            .joinToString(",")
+
+        val googleCgiEntryList
+            = URL("http://www.google.com/transliterate?langpair=ja-Hira|ja&text="
+                  + encodedHiraganaList)
             .openStream().reader().buffered().use {
-          parseJson(it, list(googleCgiEntry))
-        }
+              parseJson(it, list(googleCgiEntry))
+            }
 
         googleCgiEntryList.map { it.kanjiList.first() }
       }
