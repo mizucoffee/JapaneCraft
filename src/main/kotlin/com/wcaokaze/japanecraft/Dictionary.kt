6b1697ec32ae5a4b9d9bfb4fca8d2ae692318fee
wcaokaze <wcaokaze@gmail.com>
2017-07-01 05:52:43 +0900

    fixed a bug

--- src/main/kotlin/com/wcaokaze/japanecraft/Dictionary.kt
+++ src/main/kotlin/com/wcaokaze/japanecraft/Dictionary.kt
@@ -6,7 +6,7 @@ class Dictionary(map: Map<String, String>) {
   operator fun get(rawString: String): Pair<String, IntRange>? {
     fun loop(prevNode: Trie<String>, index: Int): Pair<String, IntRange>? {
       if (index !in rawString.indices) return null
-      val node = trie[rawString[index]] ?: return null
+      val node = prevNode[rawString[index]] ?: return null
 
       if (node.value != null) {
         return loop(node, index + 1) ?: Pair(node.value!!, 0..index)
